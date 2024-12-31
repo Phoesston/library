79a83ac2961ab4140995b889d51f43b9790bc219
Phoesston <joshua.jr2017@gmail.com>
2024-12-31 10:24:23 -0600

    finished dessign for dialog form

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const myLibrary = [];
 
-function Book(title, author){
+function Book(title,author,pages,read){
     this.title = title;
     this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(title, author){
     return myLibrary.push(new Book(title, author))
 
 }
+
+const addBookButton = document.querySelector(".add-book button"); // Select the button inside .add-book
+const formBlock = document.getElementById("add-book-form");
+const formDialog = document.querySelector(".form-dialog")
+
+addBookButton.addEventListener("click", function () {
+   formDialog.showModal();
+});
+
+const closeButton = document.querySelector(".dialog-close-button");
+closeButton.addEventListener("click", function () {
+   formDialog.close();
+});
