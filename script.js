d5eaf923690ff1fc3b98146badb76e5967d4d043
Phoesston <joshua.jr2017@gmail.com>
2025-01-03 17:23:23 -0600

    finished library project

--- script.js
+++ script.js
@@ -7,13 +7,44 @@ function Book(title,author,pages,read){
     this.read = read;
 }
 
-function addBookToLibrary(title, author){
-    return myLibrary.push(new Book(title, author))
+function addBookToLibrary(title, author,pages,read){
+    return myLibrary.push(new Book(title, author,pages, read))
 
 }
 
+function displayBooks(){
+    const mainContainer = document.getElementById("books-container")
+    mainContainer.innerHTML = ""
+
+    myLibrary.forEach((book, index) =>{
+        const card = document.createElement('div');
+        card.classList.add("card");
+        card.innerHTML = `
+            <h3>${book.title}</h3>
+            <p>by ${book.author}</p>
+            <p>${book.pages} pages</p>
+            <p>${book.read ? "Read" : "Not Read Yet"}</p>
+            <button onclick="removeBook(${index})">Remove</button>
+            <button onclick="toggleReadStatus(${index})">${book.read ? "Mark as Unread" : "Mark as Read"}</button>
+        `;
+
+        mainContainer.appendChild(card);
+    
+    });
+}
+
+function removeBook(index){
+    myLibrary.splice(index, 1);
+    displayBooks();
+}
+
+function toggleReadStatus(index){
+    myLibrary[index].read = !myLibrary[index].read;
+    displayBooks();
+}
+
 const addBookButton = document.querySelector(".add-book button"); // Select the button inside .add-book
-const formBlock = document.getElementById("add-book-form");
+const formBlock = document.getElementById("book-form");
 const formDialog = document.querySelector(".form-dialog")
 
 addBookButton.addEventListener("click", function () {
@@ -24,3 +55,18 @@ const closeButton = document.querySelector(".dialog-close-button");
 closeButton.addEventListener("click", function () {
    formDialog.close();
 });
+
+formBlock.addEventListener("submit", (e) =>{
+    e.preventDefault(); //prevents default form submission to the backend
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+
+    addBookToLibrary(title,author,pages,read);
+    formBlock.reset();
+    formDialog.close();
+    displayBooks();
+    console.log(myLibrary);
+});
+
